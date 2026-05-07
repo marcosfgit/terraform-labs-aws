a7f0c2311400d6dacc2636017d6e7a26f09fe076
Marcos Freitas <mf95pt@gmail.com>
2026-05-07 15:00:04 +0100

    LAB-04-AWS Done

--- variables.tf
+++ variables.tf
@@ -9,3 +9,22 @@ variable "environment" {
   type        = string
   default     = "learning-terraform"
 }
+
+# Subnet Variables
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for subnets"
+  type        = string
+  default     = "us-east-1a"
+}
