521d0c49d3fbc401ba62e710262a66896505ef92
Marcos Freitas <mf95pt@gmail.com>
2026-05-11 14:59:35 +0100

    LAB-06-AWS Done

---
+++ lab-06-dynamic-values/variables.tf
@@ -0,0 +1,24 @@
+variable "environment" {
+  description = "Environment name for resource naming and tagging"
+  type        = string
+  default     = "production"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for our VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for subnet"
+  type        = string
+  default     = "10.0.10.0/24"
+}
+
+variable "project_name" {
+  description = "Project name for resource tagging"
+  type        = string
+  default     = "static-infrastructure"
+}
+
