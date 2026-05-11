b3fe03ee072b9a4270f2037f81a11c5092f378d4
Marcos Freitas <mf95pt@gmail.com>
2026-05-11 19:53:51 +0100

    LAB-08-AWS Done

---
+++ lab-08-count-meta-argument/main.tf
@@ -0,0 +1,22 @@
+# Basic VPC Configuration
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Individual Subnet Resources
+resource "aws_subnet" "subnet" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr_blocks[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "subnet-${count.index + 1}"
+  }
+}
